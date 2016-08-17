ecbc7d86256f925894a5724f6bfa77c47840da29
David McElligott <damcelli@up.com>
2016-08-17 12:54:56 -0500

    Removed dead code

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp        = require('gulp'), 
-  childProcess  = require('child_process'), 
-  electron      = require('electron-prebuilt');
-
-// create the gulp task
-gulp.task('start', function () { 
-  return childProcess.spawn(electron, ['--debug=5858', '.'], { stdio: 'inherit' }); 
-});
